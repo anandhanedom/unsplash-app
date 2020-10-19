d7a9414ad5de602d2b1a1f2b0c45993d3428ebf0
Anandhan <anandhanedom@gmail.com>
2020-10-19 12:49:35 +0530

    material ui package added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
 
+//Components
+import Header from './components/Header/Header.component.jsx';
+
 function App() {
-  return <div className="App">hello world!</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
