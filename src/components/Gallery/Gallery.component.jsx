8491a3af45fa4810340b3d48fa32051393e2a962
Anandhan <anandhanedom@gmail.com>
2020-10-19 17:01:01 +0530

    navbar done

---
+++ src/components/Gallery/Gallery.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+//Components
+import ImageCard from '../ImageCard/ImageCard.component';
+
+const Gallery = () => (
+  <div>
+    <ImageCard />
+  </div>
+);
+
+export default Gallery;
