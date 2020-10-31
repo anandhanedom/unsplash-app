7d752d81d52d240a0153b406481bc736a09d7c2a
Anandhan <anandhanedom@gmail.com>
2020-10-31 17:45:06 +0530

    authentication route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 //Pages
@@ -7,7 +9,10 @@ import AuthenticationPage from './pages/auth/auth.component.jsx';
 function App() {
   return (
     <div className="App">
-      <AuthenticationPage />
+      <Switch>
+        <Route exact path="/" component={AuthenticationPage}></Route>
+        <Route path="*" component={() => <h1>404 NOT FOUND</h1>} />
+      </Switch>
     </div>
   );
 }
