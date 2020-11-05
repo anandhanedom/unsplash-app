5070ae2387aeb4d0e4df802c5d5c75c7367eb694
Anandhan <anandhanedom@gmail.com>
2020-11-05 06:51:02 +0530

    new auth

--- src/redux/auth/auth.actions.js
+++ src/redux/auth/auth.actions.js
@@ -1,111 +1,85 @@
-import axios from 'axios';
-import { AuthActionTypes } from './auth.types';
+//toggleUsers helps to toggle login and signup page in a single route
+export const toggleUser = () => ({
+  type: imageCollectionActionTypes.TOGGLE_USER_TYPE
+});
 
-//Login success
-const loginSuccess = (data) => {
-  return {
-    type: AuthActionTypes.LOGIN_SUCCESS,
-    payload: data,
+export const toggleUserAsync = () => {
+  return dispatch => {
+    dispatch(toggleUser());
   };
 };
 
-//Register success
-const registerSuccess = (data) => {
-  return {
-    type: AuthActionTypes.REGISTER_SUCCESS,
-    payload: data,
+
+// User sign Up
+​export const signUpWithCredentialAsync = (userName, password) => {
+  return async dispatch => {
+    let response;
+    await axios
+      .get("/signup")
+      .then(res => {
+        response = res;
+      })
+      .catch(err => alert("Error Hitting : ", err));
+    localStorage.setItem("access_token", response.data.access_token);
+    localStorage.setItem("refresh_token", response.data.refresh_token);
+    dispatch(addUserDetailsToStore(userName));
   };
 };
-
-//Login fail
-const loginFailure = () => {
-  return {
-    type: AuthActionTypes.LOGIN_FAIL,
+​
+// User login
+export const addUserDetailsToStore = user => ({
+  type: imageCollectionActionTypes.ADD_USER,
+  payload: user
+});
+​
+export const loginWithCredentialsAsync = (userName, password) => {
+  return async dispatch => {
+    let response;
+​
+    await axios
+      .get("/login", { email: userName, password: password })
+      .then(res => {
+        response = res.data;
+      });
+​
+    localStorage.setItem("access_token", response.access_token);
+    localStorage.setItem("refresh_token",response.refresh_token)
+    dispatch(addUserDetailsToStore(userName));
   };
 };
-
-//Register fail
-const registerFailure = () => {
-  return {
-    type: AuthActionTypes.REGISTER_FAIL,
-  };
-};
-
-//Load success
-const loadUserSuccess = (data) => {
-  return {
-    type: AuthActionTypes.USER_LOADED,
-    payload: data,
-  };
-};
-
-//Load failure
-const authError = () => {
-  return {
-    type: AuthActionTypes.AUTH_ERROR,
-  };
-};
-
-//Logout
-export const logout = () => {
-  return {
-    type: AuthActionTypes.LOGOUT,
-  };
-};
-
-//Load user
-export const loadUser = () => {
-  return async (dispatch) => {
-    //load token into global header
-    try {
-      const res = await axios.get('600/users/1');
-      dispatch(loadUserSuccess(res.data));
-    } catch (error) {
-      dispatch(authError());
-    }
-  };
-};
-
-//Register User
-export const register = (formData) => {
-  return async (dispatch) => {
-    const config = {
+​
+// Login with refresh token
+export const loginWithRefreshToken = async refresh_token => {
+  let response;
+  await axios
+    .get("/login", {
       headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-
-    const body = JSON.stringify(formData);
-
-    try {
-      const res = await axios.post('signup', body, config);
-
-      dispatch(registerSuccess(res.data));
-    } catch (err) {
-      console.log(err);
-      dispatch(registerFailure());
-    }
-  };
+        Authorization: refresh_token
+      }
+    })
+    .then(res => (response = res.data));
+​
+  localStorage.setItem("access_token", response.access_token);
+  localStorage.setItem("refresh_token",response.refresh_token)
 };
-
-//Login User
-export const login = (formData) => {
-  return async (dispatch) => {
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-
-    const body = JSON.stringify(formData);
-
-    try {
-      const res = await axios.post('login', body, config);
-
-      dispatch(loginSuccess(res.data));
-    } catch (err) {
-      console.log(err);
-      dispatch(loginFailure());
+​
+// user Logout
+​
+export const removeUserFromStore = () => ({
+  type: imageCollectionActionTypes.REMOVE_USER
+});
+​
+export const logoutAsync = () => {
+  return async dispatch => {
+    let response;
+    await axios.get("/logout", { headers: headers }).then(res => {
+      response = res;
+    });
+    if (response.status === 200 && response.data.Authorization === "") {
+      localStorage.removeItem("refresh_token");
+      localStorage.removeItem("access_token");
+    } else {
+      alert("Something went wrong... please try again");
     }
   };
 };
